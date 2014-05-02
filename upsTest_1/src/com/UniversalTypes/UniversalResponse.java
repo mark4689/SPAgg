57673471689ea13c43930cd2fe11f44be49c7fe5
Brendan Burbules <bburbules@gmail.com>
2014-05-02 19:45:51 -0400

    yes

---
+++ upsTest_1/src/com/UniversalTypes/UniversalResponse.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.UniversalTypes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mak5278
+ */
+public class UniversalResponse {
+    private ArrayList<SingleResponse> responseList;
+
+    /**
+     * @return the responseList
+     */
+    public ArrayList<SingleResponse> getResponseList() {
+        return responseList;
+    }
+
+    /**
+     * @param responseList the responseList to set
+     */
+    public void setResponseList(ArrayList<SingleResponse> responseList) {
+        this.responseList = responseList;
+    }
+    
+}
